f19a6d86922d469d47c3d07fba76e2a39a12cae0
mekdmehta1997-dev <mekdmehta1997@gmail.com>
2025-12-31 21:06:14 +0530

    Update train.py

--- training/train.py
+++ training/train.py
@@ -3,26 +3,43 @@ import mlflow.sklearn
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.ensemble import RandomForestRegressor
 
 print("Training Started...")
 
-# ---- IMPORTANT ----
 mlflow.set_experiment("house-price-prediction")
-# -------------------
 
-# Dummy example dataset
 df = pd.read_csv("/app/data/california.csv")
 
-X = df.drop("median_house_value", axis=1)
+# Target
 y = df["median_house_value"]
+X = df.drop("median_house_value", axis=1)
+
+# Identify categorical vs numeric
+categorical_cols = X.select_dtypes(include=["object"]).columns
+numeric_cols = X.select_dtypes(exclude=["object"]).columns
+
+preprocess = ColumnTransformer(
+    transformers=[
+        ("cat", OneHotEncoder(handle_unknown="ignore"), categorical_cols),
+        ("num", "passthrough", numeric_cols)
+    ]
+)
+
+model = Pipeline(steps=[
+    ("preprocess", preprocess),
+    ("rf", RandomForestRegressor())
+])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-model = RandomForestRegressor()
 model.fit(X_train, y_train)
 
 pred = model.predict(X_test)
+
 mse = mean_squared_error(y_test, pred)
 r2 = r2_score(y_test, pred)
 
@@ -32,7 +49,6 @@ print("R2 :", r2)
 with mlflow.start_run():
     mlflow.log_metric("mse", mse)
     mlflow.log_metric("r2", r2)
-
     mlflow.sklearn.log_model(model, "model")
 
-print("Training completed")
+print("Training Completed")
